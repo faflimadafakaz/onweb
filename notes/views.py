9bc297b15e9b044647abbb9671c273398649fa24
Suraj Patil <sapatil@live.com>
2015-02-28 18:41:31 +0530

    links now render pages

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import redirect
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return HttpResponse("you are logging out")
+
+def register(request):
+    return HttpResponse("register")
+
+def add_note(request, note_slug):
+    return HttpResponse("adding note"+ note_slug)
+
+def delete_note(request, note_slug):
+    return HttpResponse('delete note'+ note_slug)
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def uncategorized(request):
+    return render(request, 'home.html')
+
+def trash(request):
+    return render(request, 'home.html')
+
+def reminders(request):
+    return render(request, 'home.html')
