f7aa53fe4c609b0d918cc7caca3ca7cbb33c804d
Suraj Patil <sapatil@live.com>
2015-02-28 13:35:03 +0530

    added backend as mysql

--- onweb/settings.py
+++ onweb/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'onweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onweb',
+        'USER':'root',
+        'PASSWORD':'suraj',
+        'HOST':'127.0.0.1',
+        'PORT':'',
     }
 }
 
