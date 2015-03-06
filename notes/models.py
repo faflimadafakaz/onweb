3aacdb59f86066bc0409b826d212322195f490ee
Suraj Patil <sapatil@live.com>
2015-03-06 11:55:15 +0530

    fetch notes from db

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField, ForeignKey
+
+from datetime import datetime
+from django.utils.text import slugify
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+   
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.CharField(max_length=50, primary_key=True)
+    #colour = models.IntegerField(required=False)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    def __unicode__(self):
+        return self.name
+    
+class UserProfile(models.Model):
+    user = OneToOneField(User)
+    
+
+class Notes(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    content = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category)
+    user = models.ForeignKey(UserProfile)
+    #reminder = models.DateTimeField(default='',null=True)
+    permalink = models.CharField(max_length=50, null=False)
+    
+    def __unicode__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.modified = datetime.now()
+        super(Notes, self).save(*args, **kwargs)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
--- notes/views.py
+++ notes/views.py
@@ -2,19 +2,51 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.contrib.auth import login, authenticate, logout
+
+from notes.forms import UserCreateForm, AuthenticateForm
+from notes.models import Notes,UserProfile, Category
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, auth_form=None, user_form=None):
+    if request.user.is_authenticated():
+         user = request.user
+         uuser = UserProfile.objects.filter(user=user.id)[0]
+         notes = Notes.objects.all().order_by('-created').filter(user=uuser)
+         categories=  Category.objects.all()
+         return render(request, 'home.html', {'notes':notes, 'user':user, 'categories':categories})
+    else:
+         auth_form = auth_form or  AuthenticateForm()
+         user_form =  user_form or UserCreateForm()
+         return render(request, 'login.html', {'user_form':user_form, 'auth_form':auth_form}) 
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method=='POST':
+        form = AuthenticateForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+        else:
+            return home(request, auth_form=form)
+    return redirect('/')
 
-def logout(request):
-    return HttpResponse("you are logging out")
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def register(request):
-    return HttpResponse("register")
+    user_form = UserCreateForm(data=request.POST)
+    if request.method=='POST':
+        if user_form.is_valid():
+            username = user_form.clean_username()
+            password = user_form.clean_password2()
+            user_form.save()
+            user=authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return home(request, user_form=user_form)
+    return redirect('/')
 
 def add_note(request, note_slug):
     return HttpResponse("adding note"+ note_slug)
