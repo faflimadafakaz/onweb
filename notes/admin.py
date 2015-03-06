89f2ca752603dde3c9338cca91af5df77ef0b135
Suraj Patil <sapatil@live.com>
2015-03-06 11:54:21 +0530

    added admin prompt, will be removed later

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from notes.models import Notes, Category, UserProfile
 # Register your models here.
+
+admin.site.register(Notes)
+admin.site.register(Category)
+admin.site.register(UserProfile)
